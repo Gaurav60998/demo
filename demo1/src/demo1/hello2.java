d74dcfd22721f3e3ec7f9101741d99524a88508f
sdac-12 <sdac-12@sdac-12-PC>
2024-01-16 02:44:56 +0530

    create second java class

---
+++ demo1/src/demo1/hello2.java
@@ -0,0 +1,9 @@
+package demo1;
+
+import javax.swing.plaf.synth.SynthSeparatorUI;
+
+public class hello2 {
+public static void main(String[] args) {
+	System.out.println("create second java class");
+}
+}
