7f692f407a6bb03609895a34c669676a1bff3084
sdac-12 <sdac-12@sdac-12-PC>
2024-01-16 03:55:12 +0530

    demo 3.0

--- demo1/src/demo1/hello2.java
+++ demo1/src/demo1/hello2.java
@@ -5,5 +5,6 @@ import javax.swing.plaf.synth.SynthSeparatorUI;
 public class hello2 {
 public static void main(String[] args) {
 	System.out.println("create second java class");
+	System.out.println("gaurav");
 }
 }
